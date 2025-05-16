e566f1c1b68679a0a1e66fdaa0dd9bb036736c9b
Jordi <jordism91@gmail.com>
2025-05-16 13:56:43 +0200

    Add google map library

--- src/Home/ui/pages/HomePage.tsx
+++ src/Home/ui/pages/HomePage.tsx
@@ -1,3 +1,22 @@
-export const HomePage = () => {
-  return <></>
+import { APIProvider, Map } from '@vis.gl/react-google-maps'
+
+import { cn } from '@/lib/utils'
+
+const MEXICO_CITY_CENTER = { lat: 19.4326, lng: -99.1719 }
+
+export function Component() {
+  return (
+    <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+      <Map
+        className={cn('aspect-video w-full')}
+        defaultZoom={12}
+        defaultCenter={MEXICO_CITY_CENTER}
+        gestureHandling="greedy"
+        clickableIcons={false}
+        disableDefaultUI={true}
+      />
+    </APIProvider>
+  )
 }
+
+Component.displayName = 'HomePage'
