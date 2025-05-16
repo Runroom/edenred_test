1c46140d23636cf1d86516b03d33c89eb6a6f167
Jordi <jordism91@gmail.com>
2025-05-16 16:49:49 +0200

    Change dataset to one from Santiago de Chile

--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -7,14 +7,14 @@ test('homepage business count and search', async ({ page }) => {
   const sidebarCount = page.getByTestId('business-count')
   const initialText = await sidebarCount.textContent()
 
-  expect(initialText).toBe('10 resultados')
+  expect(initialText).toBe('30 resultados')
 
   // Type a search term in the search bar
   const searchInput = page.getByPlaceholder('Buscar comercios...')
-  await searchInput.fill('Café')
+  await searchInput.fill('Café Patagonia')
   await page.waitForTimeout(500)
 
   // Assert that the count is 1
   const newText = await sidebarCount.textContent()
-  expect(newText).toBe('1 resultado para "Café"')
+  expect(newText).toBe('1 resultado para "Café Patagonia"')
 })
